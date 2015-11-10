d695dab842e683d83e5bd0abdf1ecd518043f0e1
sethcorrell77 <sethcorrell77@gmail.com>
2015-11-09 20:30:03 -0600

    Done

---
+++ controller.js
@@ -0,0 +1,7 @@
+angular.module("userProfiles").controller("MainController", function($scope, mainService) {
+	$scope.getUsers = function() {
+		$scope.users = mainService.getUsers();
+	}
+	$scope.getUsers();
+
+})
